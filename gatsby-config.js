00413d6783a4388f148b8f5ff23cd902eb080b7c
orishko <evgeniy.orishko@litslink.com>
2020-04-13 16:20:08 +0300

    first test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,77 +1,145 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: "Gatsby + Netlify CMS Starter",
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
+        name: "uploads"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+        name: "pages"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-relative-images',
+            resolve: "gatsby-remark-relative-images",
             options: {
-              name: 'uploads',
-            },
+              name: "uploads"
+            }
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
+              maxWidth: 2048
+            }
           },
           {
-            resolve: 'gatsby-remark-copy-linked-files',
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-              destinationDir: 'static',
-            },
-          },
-        ],
-      },
+              destinationDir: "static"
+            }
+          }
+        ]
+      }
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
     },
+
     {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName:
+          process.env.PRISMIC_REPOSITORY_NAME || "prismic-gatsby-test-melius",
+        accessToken:
+          process.env.PRISMIC_ACCESS_TOKEN ||
+          "MC5YcFJURVJBQUFPN0czMGZ1.77-9Cihg77-9AT1177-977-9Uu-_vT7vv71k77-977-9Ru-_ve-_vSEh77-9CO-_ve-_vQ_vv71DbEJf",
+        linkResolver: ({ node, key, value }) => doc => {
+          // Your link resolver
+        },
+
+        // Set a list of links to fetch and be made available in your link
+        // resolver function.
+        // See: https://prismic.io/docs/javascript/query-the-api/fetch-linked-document-fields
+        fetchLinks: [
+          // Your list of links
+        ],
+
+        // Set an HTML serializer function used to process formatted content.
+        // Fields with rich text formatting use this function to generate the
+        // correct HTML.
+        // The document node, field key (i.e. API ID), and field value are
+        // provided to the function, as seen below. This allows you to use
+        // different HTML serializer logic for each field if necessary.
+        // See: https://prismic.io/docs/nodejs/beyond-the-api/html-serializer
+        htmlSerializer: ({ node, key, value }) => (
+          type,
+          element,
+          content,
+          children
+        ) => {
+          // Your HTML serializer
+        },
+
+        // Provide an object of Prismic custom type JSON schemas to load into
+        // Gatsby. This is required.
+        schemas: {
+          plans: require('./src/schemas/plans.json'),
+          // Your custom types mapped to schemas
+        },
+
+        // Set a default language when fetching documents. The default value is
+        // '*' which will fetch all languages.
+        // See: https://prismic.io/docs/javascript/query-the-api/query-by-language
+        lang: "*",
+
+        // Set a function to determine if images are downloaded locally and made
+        // available for gatsby-transformer-sharp for use with gatsby-image.
+        // The document node, field key (i.e. API ID), and field value are
+        // provided to the function, as seen below. This allows you to use
+        // different logic for each field if necessary.
+        // This defaults to always return false.
+        shouldDownloadImage: ({ node, key, value }) => {
+          // Return true to download the image or false to skip.
+          return false
+        },
+
+        // Set the prefix for the filename where type paths for your schemas are
+        // stored. The filename will include the MD5 hash of your schemas after
+        // the prefix.
+        // This defaults to 'prismic-typepaths---${repositoryName}'.
+        typePathsFilenamePrefix:
+          "prismic-typepaths---prismic-gatsby-test-melius"
+      }
+    },
+
+    {
+      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
-      },
+        purgeOnly: ["/all.sass"] // applies purging only on the bulma css file
+      }
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
-}
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
+  ]
+};
